274f2a5003f8d88687a505dc1e55153e46b8dcc2
DavideIsaia <davide.enjoy@hotmail.it>
2022-07-06 15:51:08 +0200

    aggiunto soft delete del singolo post

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,5 +5,10 @@
 <h5 class="text-muted">Slug : {{ $post->slug }}</h5>
 <p>{{ $post->content }}</p>
 
-<a href="{{ route('admin.posts.edit', ['post' => $post->id])}}" class="btn btn-primary">Modifica</a>    
+<a href="{{ route('admin.posts.edit', ['post' => $post->id])}}" class="btn btn-primary">Modifica</a>
+<form class="d-inline-block" action="{{ route('admin.posts.destroy', [ 'post' => $post->id ]) }}" method="POST" onClick="return confirm('Sei sicuro? (il post non verrà più visualizzato sul browser ma sarà ancora visibile sul database)');">
+  @csrf
+  @method('DELETE')
+  <button type="submit" class="btn btn-danger">Cancella</button>
+</form>    
 @endsection
