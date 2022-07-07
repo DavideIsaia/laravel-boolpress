4f109021926e4e5ae378e93a6dc0de9268f9e894
DavideIsaia <davide.enjoy@hotmail.it>
2022-07-07 16:57:54 +0200

    creata e collegata tabella user_info al singolo user

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
     <h2>Ciao {{ $user->name }}! Questa è la tua home di Backoffice</h2>
+    <h4>Indirizzo: {{ $userInfo->address }}</h4>
+    <h4>Telefono: {{ $userInfo->phone }}</h4>
 @endsection
