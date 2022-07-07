18ca790b67e25388012a3c74aacd19d66e6d290f
DavideIsaia <davide.enjoy@hotmail.it>
2022-07-07 16:37:33 +0200

    aggiunto font awesome

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,3 +18,5 @@ mix.js('resources/js/app.js', 'public/js')
     });
 
 mix.js('resources/js/front.js', 'public/js');
+
+mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts');
