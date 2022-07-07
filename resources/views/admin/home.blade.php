21a3faa8a604d1b553dd4a49c208e0f0810a42ad
DavideIsaia <davide.enjoy@hotmail.it>
2022-07-07 17:00:42 +0200

    aggiunto controllo if in pagina nel caso non ci siano user info da visualizzare

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <h2>Ciao {{ $user->name }}! Questa è la tua home di Backoffice</h2>
-    <h4>Indirizzo: {{ $userInfo->address }}</h4>
-    <h4>Telefono: {{ $userInfo->phone }}</h4>
+    @if ($userInfo)
+        <h4 class="text-muted">Indirizzo: {{ $userInfo->address }}</h4>
+        <h4 class="text-muted">Telefono: {{ $userInfo->phone }}</h4>
+    @endif
 @endsection
