f90438c3dba155b1aa515f777e29bb390345fb98
DavideIsaia <davide.enjoy@hotmail.it>
2022-07-11 15:45:10 +0200

    implementata funzione show dei tags

--- resources/views/admin/tags/show.blade.php
+++ resources/views/admin/tags/show.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <h1>{{ $tag->name }}</h1>
 <h5 class="text-muted">Slug: {{ $tag->slug }}</h5>
-{{-- <div class="row row-cols-4">
+<div class="row row-cols-4">
   @forelse ($tag->posts as $post)
     <div class="col">
       <div class="card mb-4">
         <div class="card-body">
           <h5 class="card-title">{{ $post->title }}</h5>
-          <a href="{{ route('admin.posts.show', ['post' =>$post->slug]) }}" class="btn btn-primary">Visualizza</a>
+          <a href="{{ route('admin.posts.show', ['slug' =>$post->slug]) }}" class="btn btn-primary">Visualizza</a>
         </div>
       </div>
     </div>
@@ -22,6 +22,6 @@
     </div>
   </div>
   @endforelse
-</div> --}}
+</div>
     
 @endsection
