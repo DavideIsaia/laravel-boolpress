7a3e8ac52d16a36ed5d7eb451aa1226462eba33d
DavideIsaia <davide.enjoy@hotmail.it>
2022-07-16 12:11:42 +0200

    aggiunta validazione di invio email dei clienti

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -7,16 +7,35 @@ use App\Lead;
 use App\Mail\NewContactRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request)
     {
         $data = $request->all();
+
+        $validator = Validator::make($data, [ //usare Support\Facades e non Dotnev\Validator
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
         $lead = new Lead();
         $lead->fill($data);
         $lead->save();
 
         Mail::to('admin@boolpress.it')->send(new NewContactRequest($lead));
+
+        return response()->json([
+            'success' => true
+        ]);
     }
 }
